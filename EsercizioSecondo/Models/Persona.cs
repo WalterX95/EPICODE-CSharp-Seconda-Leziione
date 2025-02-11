9491dc07ab3765316dc7a2854f1d9e7c3a338997
WalterX95 <walter_1995@hotmail.it>
2025-02-11 15:05:46 +0100

    Secondo Esercizio Completato

---
+++ EsercizioSecondo/Models/Persona.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EsercizioSecondo.Models
+{
+    public class Persona
+    {
+        private string _nome = string.Empty;
+        private string _cognome = string.Empty;
+        private int _eta;
+
+        public string Nome { get { return _nome; } set { _nome = value; } }
+        public string Cognome { get { return _cognome; } set { _cognome = value; } }
+
+        public int Eta { get { return _eta; } set { _eta = value; } }
+
+     
+        public string GetNome()
+        {
+            return Nome;  
+        }
+
+        public string GetCognome()
+        {
+            return Cognome;
+        }
+
+        public int GetEta()
+        {
+            return Eta;
+        }
+
+        public void GetDettagli()
+        {
+            Console.WriteLine("Nome : " + GetNome());
+            Console.WriteLine("Cognome :" + GetCognome());
+            Console.WriteLine("Età : " + GetEta());
+        }
+
+    }
+}
---
+++ EsercizioSecondo/Program.cs
@@ -0,0 +1,13 @@
+﻿using EsercizioSecondo.Models;
+
+Persona persona = new Persona();
+
+persona.Nome = "Walter";
+persona.Cognome = "Antonelli";
+persona.Eta = 29;
+
+persona.GetNome();
+persona.GetCognome();
+persona.GetEta();
+
+persona.GetDettagli();
